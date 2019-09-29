7ec7e93486e72dbb891a4c945edec6216ab50477
Wanwan <wanwanzainaer@gmail.com>
2019-09-28 20:10:27 -0700

    Add helmet seo

--- src/client/pages/UsersListPage.js
+++ src/client/pages/UsersListPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchUsers } from "../actions/index";
-
+import { Helmet } from "react-helmet";
 class UsersList extends Component {
   componentDidMount() {
     this.props.fetchUsers();
@@ -12,9 +12,19 @@ class UsersList extends Component {
       return <li key={user.id}>{user.name}</li>;
     });
   }
+
+  head() {
+    return (
+      <Helmet>
+        <title>{`${this.props.users.length} Users Loaded`}</title>
+        <meta property="og:title" content="Users App" />
+      </Helmet>
+    );
+  }
   render() {
     return (
       <div>
+        {this.head()}
         Here's a big list of users <ul>{this.renderUsers()}</ul>
       </div>
     );
--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -3,6 +3,7 @@ import { renderToString } from "react-dom/server";
 import { StaticRouter } from "react-router-dom";
 import { renderRoutes } from "react-router-config";
 import serialize from "serialize-javascript";
+import { Helmet } from "react-helmet";
 
 import Routes from "../client/Routes";
 import { Provider } from "react-redux";
@@ -14,9 +15,15 @@ export default (req, store, context) => {
       </StaticRouter>
     </Provider>
   );
+
+  const helmet = Helmet.renderStatic();
+
   return `
     <html>
-      <head>    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+      <head>
+      ${helmet.title.toString()}
+      ${helmet.meta.toString()}
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
       </head>
       <body>
           <div id="root">${content}</div>
